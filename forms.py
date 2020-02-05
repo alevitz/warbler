2c4c3cdca0e8dc6a84413346cb5673013de20f48
Adam <adamlevitz@gmail.com>
2020-02-05 12:09:22 -0800

    Complete Step 5

--- forms.py
+++ forms.py
@@ -22,4 +22,15 @@ class LoginForm(FlaskForm):
     """Login form."""
 
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[Length(min=6)])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+class UserEditForm(FlaskForm):
+    """Form for editing user profile."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Header Image URL')
+    location = StringField('(Optional) Location')
+    bio = TextAreaField('(Optional) Bio')
+    password = PasswordField('Password', validators=[DataRequired()])
